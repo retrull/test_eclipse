002cc73d8e9fc5d745342fa490b418ef2c503935
Ryan Trull <rtrull97@gmail.com>
2018-08-21 13:40:47 -0400

    Edited Display in HelloDolly

--- Project1/src/dev1/HelloDolly.java
+++ Project1/src/dev1/HelloDolly.java
@@ -3,7 +3,7 @@ package dev1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		System.out.println("Helow Werld");
+		System.out.println("Greetings World");
 		HelloWorld.main(null);
 	}
 }
